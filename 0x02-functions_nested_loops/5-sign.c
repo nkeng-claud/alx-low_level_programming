6fcc625a993b60fbcb94922152a66821809c96f2
nkeng-claud <nkengclaud@gmail.com>
2022-09-14 18:56:52 -0700

    task 5 review

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,8 +10,8 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
-return (1);
+putchar('+1');
+return (+1);
 }
 else if (n == 0)
 {
@@ -20,7 +20,7 @@ return (0);
 }
 else
 {
-putchar('-');
+putchar('-1');
 return (-1);
 }
 }
